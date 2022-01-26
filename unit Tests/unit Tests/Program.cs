967e2372deebd8e402a487055b3e25ffc41af25a
TimothyAlban <timothyalban@trentu.ca>
2022-01-26 13:59:02 -0500

    First commit

---
+++ unit Tests/unit Tests/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unit_Tests
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+
+        public static string FlipCoin()
+        {
+            
+            Random rand = new Random();
+
+            if (rand.Next(1, 3) == 1)
+            {
+                return "heads";
+            }
+            else return "tails";
+
+        }
+    }
+}
---
+++ unit Tests/unit Tests/TestObject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace unit_Tests
+{
+    public delegate string myDelegate();
+    public class TestObject
+    {
+        //internal variables
+        private bool pass;
+        private string testMessage;
+        
+
+        // properties
+        public bool Pass
+        {
+            get { return pass; }
+
+            set { pass = value; }
+        }
+
+        public string TestMessage
+        {
+            get { return testMessage; }
+
+            set { testMessage = value; }
+        }
+
+
+        //test Functions
+
+        public bool IsHeads(){
+            if (Program.FlipCoin() == "heads")
+            {
+                return true;
+            }                                        
+
+            return false;
+        }
+    
+
+    }
+}
